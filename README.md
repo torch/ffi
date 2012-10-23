55396ebea8815aef602725f38b24e81b743e6a0d
Clement Farabet <clement.farabet@gmail.com>
2012-10-23 17:35:24 -0400

    Initial commit: very basic Tensor <> FFI bindings

---
+++ README.md
@@ -0,0 +1,74 @@
+TorchFFI
+========
+
+This package exposes Torch's Tensor and Storage data structures, through
+LuaJIT's FFI. This allows extremely fast access to Tensors and Storages, 
+all from Lua.
+
+The original neural network from Torch7, 'nn', contains stable and widely
+used modules. 'nnx' contains more experimental, unproven modules, and
+optimizations. Eventually, modules that become stable enough will make 
+their way into 'nn' (some already have).
+
+Disclaimer: DONT USE THIS PACKAGE WITHOUT FIRST CHECKING ITS MODULES !!!
+
+Installation
+------------
+
+* Install Torch7 (refer to its own [documentation](www.torch.ch)). It
+  is necessary to build it with LuaJIT, to enable FFI:
+
+```sh
+$ cmake . -D WITH_LUA_JIT=1
+$ make install
+```
+
+* Install _torchffi_:
+
+```sh
+$ torch-pkg -local install torchffi
+```
+
+Usage
+-----
+
+Simply require `torchffi`, and you'll have access to a new function in `torch`,
+called `torch.data(obj)`:
+
+```lua
+> t = torch.randn(3,2)
+> print(t)
+ 0.8008 -0.6103
+ 0.6473 -0.1870
+-0.0023 -0.4902
+[torch.DoubleTensor of dimension 3x2]
+
+> t_data = torch.data(t)
+> for i = 0,t:nElement()-1 do t_data[i] = 0 end
+> print(t)
+0 0
+0 0
+0 0
+[torch.DoubleTensor of dimension 3x2]
+```
+
+Accessing the raw data of a Tensor like this is extremely efficient, in fact, it's
+almost as fast as C in lots of cases.
+
+WARNING: bear in mind that accessing the raw data like this is dangerous, and should
+only be done on contiguous tensors (if a tensor is not contiguous, then you have to
+use it size and stride information). Making sure a tensor is contiguous is easy:
+
+```lua
+> t = torch.randn(3,2)
+> t_noncontiguous = t:transpose(1,2)
+
+-- it would be unsafe to work with torch.data(t_noncontiguous)
+
+> t_transposed_and_contiguous = t:noncontiguous:contiguous()
+
+-- it is now safe to work with the raw pointer
+
+> data = torch.data(t_contiguous)
+```
+
---
+++ init.lua
@@ -0,0 +1,74 @@
+--[======================================================================[
+   TorchFFI: FFI bindings for Torch.
+
+   This package is a pretty straightforward extension to Torch7, which
+   exposes its main data structures through FFI.
+
+   Requires LuaJIT.
+--]======================================================================]
+
+
+-- We need FFI, of course :-)
+require 'torch'
+ffi = require 'ffi'
+
+-- Generate Bindings for Storages of all types:
+local defs = {}
+for k,v in pairs(torch) do
+   if k:find('(.+)Storage') then
+      local type_storage = k
+      local type_elt = k:gmatch('(.*)Storage')():lower()
+      type_elt = type_elt:gsub('byte', 'unsigned char')
+      table.insert(defs, [[
+         typedef struct
+         {
+            ]] .. type_elt .. [[ *data;
+            long size;
+            int refcount;
+            char flag;
+         } ]] .. type_storage .. [[;
+      ]])
+   end
+end
+
+-- Generate Bindings for Tensors of all types:
+for k,v in pairs(torch) do
+   if k:find('(.+)Tensor') then
+      local type_tensor = k
+      local type_storage = k:gmatch('(.*)Tensor')() .. 'Storage'
+      table.insert(defs, [[
+         typedef struct 
+         {
+            long *size;
+            long *stride;
+            int nDimension;
+            ]] .. type_storage .. [[ *storage;
+            long storageOffset;
+            int refcount;
+            char flag;
+         } ]] .. type_tensor .. [[;
+      ]])
+   end
+end
+
+-- Load defs
+defs = table.concat(defs,'\n')
+ffi.cdef(defs)
+
+-- Method to return raw data table
+function torch.data(obj)
+   -- first cast pointer into the right type
+   local type_obj = torch.typename(obj)
+   local type_tensor = type_obj:gfind('torch%.(.*)Tensor')()
+   local type_storage = type_obj:gfind('torch%.(.*)Storage')()
+   if type_tensor then
+      -- return raw pointer to data
+      return ffi.cast(type_tensor .. 'Tensor*', torch.pointer(obj)).storage.data
+   elseif type_storage then
+      -- return raw pointer to data
+      return ffi.cast(type_storage .. 'Storage*', torch.pointer(obj)).data
+   else
+      print('Unknown data type: ' .. type_obj)
+   end
+end 
+
